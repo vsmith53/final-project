2de447f9fc1a42a31744ff6ff2d1e3292ecb28b0
vsmith53 <46981146+vsmith53@users.noreply.github.com>
2019-11-25 19:55:13 -0600

    update html & js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
 
+$(function(){
+	$('#res-btn').on("click", function(evt){
+		$('#learn-more').html('<h5>Residential</h5><p>We offer recurring services for residential neighborhoods with various add-ons! Contact us today!</p>');
+	} )
+})
+
+$(function(){
+	$('#comm-btn').on("click", function(evt){
+		$('#learn-more').html('<h5>Commercial</h5><p>Garcia Lawn Pro can tailor a custom package specific to your business or HOA needs! Contact us today!</p>');
+	} )
+})
